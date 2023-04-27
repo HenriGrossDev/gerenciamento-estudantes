e8a6d5febb9be4027b0682241dfc69e007da8103
Henrique Grosscklauss De Souza Costa <henrique.costa@pd.tec.br>
2023-04-27 19:32:52 -0300

    Configura banco de dados em memória

---
+++ backend/GerenciamentoEstudantes/GerenciamentoEstudantes/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoEstudantes.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
